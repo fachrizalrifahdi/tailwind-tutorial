999cab72e9c6c1080cd59edd5a18fd80cda3233b
Fachrizal Rifahdi <fachrizalrifahdi@gmail.com>
2020-10-17 19:26:47 +0800

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend:{
+      colors: {
+        
+      },
+    } 
+  },
+  variants: {},
+  plugins: [],
+}
